2976fa0979b0dd8bd9ea6415632564bd3f71ef3f
Natalie Blackburn & Marty Fajardo <student@epicodus.com>
2014-04-15 12:32:01 -0700

    finish CRUD for users, add basic AJAX functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,42 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       flash[:notice] = "Signup successful."
-      redirect_to root_url
+      respond_to do |format|
+        format.html { redirect_to root_url }
+        format.js
+      end
     else
       flash[:alert] = "Signup failure. Please try again."
       render 'new'
     end
   end
 
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def show
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+    if @user.update user_params
+      flash[:notice] = "User has been updated!"
+      redirect_to root_url
+    else
+      flash[:alert] = "User not updated. Please try again."
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find params[:id]
+    if @user.destroy
+      flash[:notice] = "User has been deleted."
+      redirect_to root_url
+    end
+  end
 
 private
   def user_params
